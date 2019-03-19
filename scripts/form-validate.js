9158fcac98cb17305449f5c51d173fc5eda8f8b5
Damon <damontfrancisco@yahoo.com>
2019-03-18 23:05:26 -0500

    Email form on hold for now

--- scripts/form-validate.js
+++ scripts/form-validate.js
@@ -1,33 +1,16 @@
 "use strict";
 
-window.addEventListener("load", function() {
-    function sendData() {
-        //const formData = new FormData(e.target);
-        //console.log(formData);
-        var XHR = new XMLHttpRequest();
-        var FD  = new FormData(form);
+var form = document.querySelector('form');
 
-        // Define what happens on successful data submission
-        XHR.addEventListener('load', function(event) {
-            console.log('Yeah! Data sent and response loaded.');
-        });
-
-        // Define what happens in case of error
-        XHR.addEventListener('error', function(event) {
-            console.log('Oops! Something went wrong.');
-        });
-
-        // Set up our request
-        XHR.open('POST', 'http://localhost:8000/pages/cors.php', true);
-
-        // Send our FormData object; HTTP headers are set automatically
-        XHR.send(FD);
-    }
-
-    var form = document.querySelector('form');
-
-    form.addEventListener('submit', ( e ) => {
-        e.preventDefault();
-        sendData();
-    });
+form.addEventListener('submit', ( e ) => {
+    e.preventDefault();
+    /*var name = form.querySelector('input');
+    var email = document.getElementById('email');
+    var subject = document.getElementById('subject');
+    var message = document.getElementById('msg');
+    console.log(name.value);
+    console.log(email.value);
+    console.log(subject.value);
+    console.log(message.value);*/
+    emailjs.sendForm('default_service', 'form', this);
 });
